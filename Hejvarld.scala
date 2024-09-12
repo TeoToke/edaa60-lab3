37a1ac8c072d8d35897316132887e3c2d41b507e
TerminalTeo <teo@raitio.se>
2024-09-12 09:27:39 +0200

    Jag ändrade igen :))

--- Hejvarld.scala
+++ Hejvarld.scala
@@ -1,1 +1,6 @@
-object Helloworld {def main(args: Array[String]): Unit = {println("Hej, varld!")}}
+object Helloworld {def main(args: Array[String]): Unit = {
+    def hej = println("Hej, varld!")
+    def start = for (i <- 1 to 10) {hej}
+    start
+}
+}
