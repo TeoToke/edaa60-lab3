87461d75488105ee97aa879477ee3638f07db608
TerminalTeo <teo@raitio.se>
2024-09-12 09:45:53 +0200

    To the moon baby!

--- Hejvarld.scala
+++ Hejvarld.scala
@@ -1,5 +1,5 @@
 object Helloworld {def main(args: Array[String]): Unit = {
-    def hej = println("Hejdå, varld!")
+    def hej = println("Hejdå, varld! Jag ska till månen!")
     def start = for (i <- 1 to 10) {hej}
     start
 }
